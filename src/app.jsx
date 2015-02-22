5af45f1530dcbd61d3c23aaf28939c0a0203b464
codeaotc <richgorman@live.co.uk>
2015-02-22 09:16:09 +0000

    change to display list of items

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,52 @@
-var React = require('react');
+var React = require('react'),
+    people = [
+        {
+            name: 'geoff',
+            job: 'machine man'
+        },
+        {
+            name: 'bill',
+            job: 'wood cutter'
+        }
+    ];
+
+var ListItem = React.createClass({
+    render: function() {
+        return (
+            <li>
+                <strong>{this.props.name}: </strong>
+                {this.props.job}
+            </li>
+        )
+    }
+})
+
+var List = React.createClass({
+    getInitialState: function() {
+        return {people: people}
+    },
+    render: function() {
+        return (
+            <ul>
+                {
+                    this.state.people.map(function(person) {
+                        return (
+                            <ListItem name={person.name} job={person.job}/>
+                        )
+                    })
+                }
+            </ul>
+        )
+    }
+})
 
 var App = React.createClass({
-    render() {
-        return <h1>Hi</h1>;
-    }
-});
 
-React.render(<App/>, document.getElementById('example'));
+    render: function() {
+        return (
+            <List></List>
+        )
+    }
+})
+
+React.render(<App/>, document.getElementById('app'));
